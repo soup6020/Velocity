5fc1e8dfadf16bbb387b3dbc146a43d2c404fe99
Five (Xer) <admin@fivepb.me>
2021-02-18 22:02:26 +0100

    Snapshot 21w07a

--- proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/ClientSettings.java
+++ proxy/src/main/java/com/velocitypowered/proxy/protocol/packet/ClientSettings.java
@@ -16,12 +16,13 @@ public class ClientSettings implements MinecraftPacket {
   private byte difficulty; // 1.7 Protocol
   private short skinParts;
   private int mainHand;
+  private boolean chatFilteringEnabled; // Added in 1.17
 
   public ClientSettings() {
   }
 
   public ClientSettings(String locale, byte viewDistance, int chatVisibility, boolean chatColors,
-      short skinParts, int mainHand) {
+      short skinParts, int mainHand, boolean chatFilteringEnabled) {
     this.locale = locale;
     this.viewDistance = viewDistance;
     this.chatVisibility = chatVisibility;
@@ -81,6 +82,14 @@ public class ClientSettings implements MinecraftPacket {
     this.mainHand = mainHand;
   }
 
+  public boolean isChatFilteringEnabled() {
+    return chatFilteringEnabled;
+  }
+
+  public void setChatFilteringEnabled(boolean chatFilteringEnabled) {
+    this.chatFilteringEnabled = chatFilteringEnabled;
+  }
+
   @Override
   public String toString() {
     return "ClientSettings{"
@@ -90,6 +99,7 @@ public class ClientSettings implements MinecraftPacket {
         + ", chatColors=" + chatColors
         + ", skinParts=" + skinParts
         + ", mainHand=" + mainHand
+        + ", chatFilteringEnabled=" + chatFilteringEnabled
         + '}';
   }
 
@@ -108,6 +118,10 @@ public class ClientSettings implements MinecraftPacket {
 
     if (version.compareTo(ProtocolVersion.MINECRAFT_1_9) >= 0) {
       this.mainHand = ProtocolUtils.readVarInt(buf);
+
+      if (version.compareTo(ProtocolVersion.MINECRAFT_1_17) >= 0) {
+        this.chatFilteringEnabled = buf.readBoolean();
+      }
     }
   }
 
@@ -129,6 +143,10 @@ public class ClientSettings implements MinecraftPacket {
 
     if (version.compareTo(ProtocolVersion.MINECRAFT_1_9) >= 0) {
       ProtocolUtils.writeVarInt(buf, mainHand);
+
+      if (version.compareTo(ProtocolVersion.MINECRAFT_1_17) >= 0) {
+        buf.writeBoolean(chatFilteringEnabled);
+      }
     }
   }
 
